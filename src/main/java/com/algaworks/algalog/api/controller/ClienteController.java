a2a47ea6f4f94ead93a1dc4ef05f0afac8403b6d
Vinicius <viniciusmoreira92@live.com>
2021-05-19 22:12:40 -0300

    Criando ClienteRepository, utilizando o Spring Data JPA e fazend queries

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -2,24 +2,32 @@ package com.algaworks.algalog.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor //gera um construtor que contem todas as propriedades da classe
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager; //Realiza operacoes com as entidades relacionadas a ele. Como insert, update, etc
+//	@Autowired //Srping Data JPA fornece a implementacao da interface permitindo a utilizacao dos metodos proprios e herdados
+	private ClienteRepository clienteRepository;
 	
+	/*
+	public ClienteController(ClienteRepository clienteRepository) { //Injeta o cliente repository atraves do construtor da classe
+		super();
+		this.clienteRepository = clienteRepository;
+	} */
+
 	@GetMapping("/clientes") //Mapeia o método listar na chamada de clientes
 	public List<Cliente> listar() {
 		
-		return manager.createQuery("from Cliente", Cliente.class).getResultList();
+		return clienteRepository.findByNomeContaining("a");
 	}
 	
 }
---
+++ src/main/java/com/algaworks/algalog/domain/repository/ClienteRepository.java
@@ -0,0 +1,19 @@
+package com.algaworks.algalog.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.algaworks.algalog.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	//Spring Data JPA fornece a pesquisa em tempo de execucão, para que a consulta seja bem sucedida os metodos devem seguir o padrao do Spring Data JPA
+	
+	List<Cliente> findByNome(String nome);
+
+	List<Cliente> findByNomeContaining(String nome); //Pesquisa nao exata, como o "like" do sql
+	
+}
