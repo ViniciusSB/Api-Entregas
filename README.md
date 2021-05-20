493ffbdc5e1f5ebece214d0f3031662993de6f1b
Vinicius <viniciusmoreira92@live.com>
2021-05-19 21:32:17 -0300

    Mapeando a tabela cliente com o Jakarta Percistence (JPA)

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # API - Entregas
 
-Projeto desenvolvido durante o curso Mergulho Spring REST.
+- Projeto desenvolvido durante o curso Mergulho Spring REST.
 
-Tecnologias utilizadas: Spring Framework e Postman. 
+- Tecnologias utilizadas: Spring Framework e Postman. 
 
--Diagrama de classes:
+- Diagrama de classes:
 <img src="https://github.com/ViniciusSB/Api-Entregas/blob/master/src/main/resources/images/readme/uml%20api-pedidos.png" height="400px" width="600">
--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,22 +13,13 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager; //Realiza operacoes com as entidades relacionadas a ele. Como insert, update, etc
+	
 	@GetMapping("/clientes") //Mapeia o método listar na chamada de clientes
 	public List<Cliente> listar() {
 		
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Ricardo");
-		cliente1.setTelefone("58 99999-9999");
-		cliente1.setEmail("ricardoneves@gmail.com");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("61 88888-8888");
-		cliente2.setEmail("mariajoaquina@gmail.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 	
 }
